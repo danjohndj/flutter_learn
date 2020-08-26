552775e97e06a79fe984994cdce41c2d2f5f1ec2
Dan Johnson <danjohndj@hotmail.com>
2020-08-26 13:38:30 -0400

    Got grid to work, have to put validation back.

---
+++ iOS/SpotifyShuffle/SpotifyShuffle/SpotifyShuffle-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SpotifyShuffle-Bridging-Header.h
+//  SpotifyShuffle
+//
+//  Created by Dan Johnson on 8/22/20.
+//  Copyright © 2020 Dan Johnson. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
