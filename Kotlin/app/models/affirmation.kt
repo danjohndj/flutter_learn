73c87fcd067e7063869d2049b6dc08ef67dabfc5
Dan Johnson <danjohndj@hotmail.com>
2021-05-05 08:59:56 -0400

    Add Order db code

---
+++ Kotlin/app/models/affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.kotlindroidpoc.models
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
---
+++ Kotlin/app/models/data/datasource.kt
@@ -0,0 +1,10 @@
+package com.example.kotlindroidpoc.models.data
+
+import com.example.kotlindroidpoc.R
+import com.example.kotlindroidpoc.models.data
+class Datasource() {
+     fun loadAffirmations(): List<Affirmation>
+    {
+        return
+    }
+}
